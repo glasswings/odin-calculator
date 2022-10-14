dca3869f55d0348f7efdd1944cf9742313de22c3
glasswings <glasswings363@proton.me>
2022-10-14 17:07:47 -0400

    Initialize with AGPL notices

---
+++ calculator.css
@@ -0,0 +1,4 @@
+/*  Part of Glasswings' Shuntyard Calculator, Copyright 2022 by Glasswings
+    You can redistribute, host, and/or modify it under the GNU Affero General Public License
+    which should be hosted alongside it and its source code, but you can also get a copy
+    of the license or later versions from https://www.gnu.org/licenses/ */
